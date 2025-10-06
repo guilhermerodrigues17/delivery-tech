9f4c52680990a48e724da1b46b435a97031a1f5d
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-10-06 18:50:09 -0300

    feat(restaurant): implements repository

---
+++ src/main/java/com/deliverytech/delivery_api/repository/RestaurantRepository.java
@@ -0,0 +1,17 @@
+package com.deliverytech.delivery_api.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.deliverytech.delivery_api.model.Restaurant;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
+
+    Optional<Restaurant> findByName(String name);
+
+    List<Restaurant> findByCategory(String category);
+
+    List<Restaurant> findByActiveTrue();
+
+}
