dcc641b40ac84f09ff9b565673913baa59f0875f
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-10-07 16:56:01 -0300

    feat(product): implements repository

---
+++ src/main/java/com/deliverytech/delivery_api/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.deliverytech.delivery_api.repository;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.deliverytech.delivery_api.model.Product;
+import java.util.List;
+
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    List<Product> findByRestaurantId(UUID restaurantId);
+
+    List<Product> findByRestaurantIdAndAvailableTrue(UUID restaurantId);
+
+    List<Product> findByCategory(String category);
+
+    List<Product> findByCategoryAndAvailableTrue(String category);
+
+    List<Product> findByAvailableTrue();
+}
