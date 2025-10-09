80d4d8337e1d8c994016f23fa7142733c0dcf36d
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-10-09 09:35:05 -0300

    fix(mapper): Add missing mapping annotations in RestaurantMapper

--- src/main/java/com/deliverytech/delivery_api/mapper/RestaurantMapper.java
+++ src/main/java/com/deliverytech/delivery_api/mapper/RestaurantMapper.java
@@ -1,6 +1,7 @@
 package com.deliverytech.delivery_api.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import com.deliverytech.delivery_api.dto.request.RestaurantRequestDto;
 import com.deliverytech.delivery_api.dto.response.RestaurantResponseDto;
 import com.deliverytech.delivery_api.model.Restaurant;
@@ -8,6 +9,10 @@ import com.deliverytech.delivery_api.model.Restaurant;
 @Mapper(componentModel = "spring")
 public interface RestaurantMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "active", constant = "true")
+    @Mapping(target = "orders", ignore = true)
+    @Mapping(target = "products", ignore = true)
     Restaurant toEntity(RestaurantRequestDto dto);
 
     RestaurantResponseDto toDto(Restaurant restaurant);
