edb712469d246c24a68bcc8efb1c60ff98a59562
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-10-09 14:53:06 -0300

    fix(product): Add price field to response dto

--- src/main/java/com/deliverytech/delivery_api/dto/response/ProductResponseDto.java
+++ src/main/java/com/deliverytech/delivery_api/dto/response/ProductResponseDto.java
@@ -1,8 +1,9 @@
 package com.deliverytech.delivery_api.dto.response;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public record ProductResponseDto(UUID id, String name, String description, String category,
-        Boolean available, String restaurantName, UUID restaurantId) {
+                Boolean available, BigDecimal price, String restaurantName, UUID restaurantId) {
 
 }
