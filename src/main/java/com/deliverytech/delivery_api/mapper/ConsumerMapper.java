4c314706b36a14bf78db140ce1b903cb9ca3b959
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-10-13 11:24:14 -0300

    fix(consumer): Refactor mapper to include mapping annotations for toEntity method

--- src/main/java/com/deliverytech/delivery_api/mapper/ConsumerMapper.java
+++ src/main/java/com/deliverytech/delivery_api/mapper/ConsumerMapper.java
@@ -4,11 +4,15 @@ import com.deliverytech.delivery_api.dto.request.ConsumerRequestDto;
 import com.deliverytech.delivery_api.dto.response.ConsumerResponseDto;
 import com.deliverytech.delivery_api.model.Consumer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ConsumerMapper {
 
     ConsumerResponseDto toDto(Consumer consumer);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    @Mapping(target = "orders", ignore = true)
     Consumer toEntity(ConsumerRequestDto dto);
 }
