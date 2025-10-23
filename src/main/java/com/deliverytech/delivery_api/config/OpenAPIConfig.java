6626c2865e79a01caecec0d61c56263364375f48
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-10-23 12:19:33 -0300

    build(swagger): Switch to Scalar and OpenAPI settings

---
+++ src/main/java/com/deliverytech/delivery_api/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.deliverytech.delivery_api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+        title = "Delivery API",
+        description = "API RESTful para o sistema de delivery da empresa Delivery Tech, " +
+                "desenvolvido como parte da prática de Spring Boot 3 e Java 21.",
+        version = "1.0.0",
+        contact = @Contact(
+                name = "Guilherme Rodrigues Machado",
+                email = "guilhermerodriguesm23@gmail.com",
+                url = "https://github.com/guilhermerodrigues17/delivery-tech"
+        )),
+        servers = {
+                @Server(
+                        description = "Ambiente de Desenvolvimento Local",
+                        url = "http://localhost:8080"
+                )
+        })
+public class OpenAPIConfig {}
