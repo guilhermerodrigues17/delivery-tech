982e6f6abcaad36b50a4c6aaf31d1231df1e0134
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-10-24 16:59:10 -0300

    docs(health): Add OpenAPI documentation for Health resource

--- src/main/java/com/deliverytech/delivery_api/controller/HealthController.java
+++ src/main/java/com/deliverytech/delivery_api/controller/HealthController.java
@@ -1,5 +1,11 @@
 package com.deliverytech.delivery_api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +14,26 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
+@Tag(name = "Informações", description = "Endpoints para informações sobre a API")
 public class HealthController {
 
+    @Operation(summary = "Health Check", description = "Retorna informações sobre a saúde e o estado do servidor.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Informações retornadas com sucesso",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            type = "object",
+                            additionalPropertiesSchema = String.class,
+                            example =
+                                    "{\"" +
+                                    "status\": \"UP\", \"" +
+                                    "timestamp\": \"2025-10-01T00:00:00Z\"" +
+                                    "}"
+                    )
+            )
+    )
     @GetMapping("health")
     public ResponseEntity<Map<String, String>> health() {
         Map<String, String> response = Map.of(
@@ -21,6 +45,15 @@ public class HealthController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Informações do servidor", description = "Retorna informações técnicas sobre o servidor.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Informações retornadas com sucesso",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = AppInfo.class)
+            )
+    )
     @GetMapping("info")
     public ResponseEntity<AppInfo> info() {
         var appInfo = new AppInfo(
@@ -34,6 +67,7 @@ public class HealthController {
         return ResponseEntity.ok(appInfo);
     }
 
+    @Schema(description = "Informações técnicas do servidor")
     public record AppInfo(
             String application,
             String version,
