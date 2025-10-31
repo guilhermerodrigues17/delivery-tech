3046d56967ee5a469555861437a3ad71536e2e4f
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-10-31 17:36:59 -0300

    docs(scalar): Add security schema with JWT token to OpenAPIConfig

--- src/main/java/com/deliverytech/delivery_api/config/OpenAPIConfig.java
+++ src/main/java/com/deliverytech/delivery_api/config/OpenAPIConfig.java
@@ -1,12 +1,20 @@
 package com.deliverytech.delivery_api.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @OpenAPIDefinition(
         info = @Info(
         title = "Delivery API",
