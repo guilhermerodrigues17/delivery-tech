ff3f0b5fa0cf265443d87c78e80d8931fde63643
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-11-03 18:35:39 -0300

    feat(order): Add all args constructor to OrderRequestDto and OrderItemRequestDto

--- src/main/java/com/deliverytech/delivery_api/dto/request/OrderItemRequestDto.java
+++ src/main/java/com/deliverytech/delivery_api/dto/request/OrderItemRequestDto.java
@@ -3,11 +3,13 @@ package com.deliverytech.delivery_api.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
 @Schema(description = "DTO de criação de itens do pedido")
 public class OrderItemRequestDto {
     @Schema(description = "ID do produto", example = "493897d6-f2f7-42a5-8847-a6335b6604f9")
