d2f86c87741e9b7524a8303f7dd3c0f3c94c8be8
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-11-04 11:02:58 -0300

    test: Add BaseIntegrationTest for integration tests
    Implement a class that configures the basic tools for implementing integration tests.

---
+++ src/test/java/com/deliverytech/delivery_api/BaseIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.deliverytech.delivery_api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class BaseIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+}
