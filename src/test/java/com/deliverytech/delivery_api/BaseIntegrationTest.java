f8a3ea09d605eecc014913a61fcd90d9b283dcee
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-11-07 16:11:07 -0300

    test(base): Add repositories injection and @BeforeEach method to clear the database

--- src/test/java/com/deliverytech/delivery_api/BaseIntegrationTest.java
+++ src/test/java/com/deliverytech/delivery_api/BaseIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.deliverytech.delivery_api;
 
+import com.deliverytech.delivery_api.repository.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,4 +21,32 @@ public class BaseIntegrationTest {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ConsumerRepository consumerRepository;
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @BeforeEach
+    void cleanDatabase() {
+        orderItemRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+        consumerRepository.deleteAllInBatch();
+        restaurantRepository.deleteAllInBatch();
+    }
 }
