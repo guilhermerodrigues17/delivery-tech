74e4c7cbfdf53fe8de00580a308236715c877fec
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-11-11 18:56:40 -0300

    feat(cache): Add CacheConfig with a basic CacheManager

---
+++ src/main/java/com/deliverytech/delivery_api/config/CacheConfig.java
@@ -0,0 +1,25 @@
+package com.deliverytech.delivery_api.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+        cacheManager.setCacheNames(List.of(
+                "restaurants",
+                "consumers",
+                "products",
+                "products_by_restaurant"
+        ));
+
+        return cacheManager;
+    }
+}
